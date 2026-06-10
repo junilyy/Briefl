b182719d14af3a811b70ad1b9ea47a3c64103262
Junyoung Lee <junily@yonsei.ac.kr>
2026-06-10 13:59:39 +0900

    chore: Spring Boot 백엔드 초기 설정 (#1)

---
+++ backend/src/main/java/com/briefl/BrieflApplication.java
@@ -0,0 +1,13 @@
+package com.briefl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrieflApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrieflApplication.class, args);
+	}
+
+}
